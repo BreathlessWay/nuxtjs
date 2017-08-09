bff95b9128322c1dcea9baf4baf9c60fcf098cc2
朱健 <zhujian@yowits.com>
2017-08-09 15:14:24 +0800

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle" style="font-size: 60px">
-        Nuxt.js project  fasfafaf
+        Nuxt.js project  fasgasafasfrerfafaf
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>

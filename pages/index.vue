a7f70d967253c7bb268504aa40173e6936e2b17c
朱健 <zhujian@yowits.com>
2017-08-09 16:09:47 +0800

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle" style="font-size: 60px">
-        webhook是收费塘沽塘沽
+        webhook哈哈哈哈哈哈哈哈哈
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>

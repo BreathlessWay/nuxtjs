c704af3f449434522c8959e3facdf330d9352838
BreathlessWay <731005087@qq.com>
2017-08-09 21:20:26 +0800

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle" style="font-size: 60px">
-        webhook实时在线发布,清理.nuxt目录
+        nginx反向代理，webhook自动发布
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>

341c4252f2bfd7580d6c5b4f414aa4da7aba0d93
朱健 <zhujian@yowits.com>
2017-08-09 16:59:09 +0800

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle" style="font-size: 60px">
-        webhook灌灌灌灌灌灌灌灌
+        webhook346的认同感等人通过通过
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>

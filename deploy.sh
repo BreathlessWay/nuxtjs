79cba8d2b31e7d7b587762c63c60be3115e00898
朱健 <zhujian@yowits.com>
2017-08-09 16:43:56 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
 
 echo "Start deployment"
 echo "pulling source code..."
-git help
-git pull
+git pull origin dev
 /root/node/lib/node_modules/npm i
 /root/node/lib/node_modules/npm run build
 /root/node/pm2 restart npm

349fe2969f12c5ca17ac721dcdc01a6e94f1339e
朱健 <zhujian@yowits.com>
2017-08-09 15:54:00 +0800

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle" style="font-size: 60px">
-        webhookhderterter
+        webhookTEST
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>

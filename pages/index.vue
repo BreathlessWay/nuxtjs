01e5e5152e31e60688be02a45e3bab777705412e
朱健 <zhujian@yowits.com>
2017-08-09 16:24:58 +0800

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
       </h1>
       <h2 class="subtitle" style="font-size: 60px">
         webhook为什么不走git命令
+      </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>

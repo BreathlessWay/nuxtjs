1f2200caf3c592bba25e2c329c7519cb94326a60
朱健 <zhujian@yowits.com>
2017-08-09 15:34:53 +0800

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle" style="font-size: 60px">
-        gsdfsf
+        webhook好麻烦啊
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>

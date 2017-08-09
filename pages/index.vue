7a6329f1a26e559f1e446bfb37afd77b3264fb83
朱健 <zhujian@yowits.com>
2017-08-09 16:08:21 +0800

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle" style="font-size: 60px">
-        webhook椰蓉36356的突然倒塌
+        webhook是收费塘沽塘沽
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>

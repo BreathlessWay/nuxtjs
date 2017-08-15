b1a888a09daa5568ab1f376fd4be258de671053f
朱健 <zhujian@yowits.com>
2017-08-15 17:23:13 +0800

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -118,7 +118,7 @@
     }
 
     .blog-index_banner {
-      background: url("../static/banner.png") no-repeat center 70px fixed;
+      background: url("../static/banner.png") @banner-background no-repeat center 70px fixed;
       background-size: cover;
       width: 100%;
       height: calc(~"100vh - 70px");

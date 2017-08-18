ee7319716448a2466611d1d124d194f3023241b4
BreathlessWay <731005087@qq.com>
2017-08-18 21:00:43 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,10 @@
 echo "Start deployment"
 echo "pulling source code..."
 pm2 stop npm
-git reset --hard origin/master
-git clean -f
 git pull
-git checkout dev
 echo "pulling success && start build"
-# npm i
-# npm run build
-# echo "build success && restart node"
+npm i
+npm run build
+echo "build success && restart node"
 pm2 restart npm
 echo "Finished."

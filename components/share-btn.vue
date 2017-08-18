fa4720064e5996d6fd3d1a32fd5a088784c17df6
朱健 <zhujian@yowits.com>
2017-08-18 13:29:02 +0800

    no message

--- components/share-btn.vue
+++ components/share-btn.vue
@@ -1,16 +1,13 @@
 <template>
   <section class="share-btn">
     <span>分享到：</span>
-    <!-- JiaThis Button BEGIN -->
     <div class="jiathis_style_32x32">
-      <a class="jiathis_button_qzone"></a>
       <a class="jiathis_button_tsina"></a>
-      <a class="jiathis_button_weixin"></a>
-      <a class="jiathis_button_huaban"></a>
       <a class="jiathis_button_douban"></a>
-      <a href="http://www.jiathis.com/share" class="jiathis jiathis_txt jtico jtico_jiathis" target="_blank" style="visibility: hidden"></a>
+      <a class="jiathis_button_huaban"></a>
+      <a class="jiathis_button_cqq"></a>
+      <a href="http://www.jiathis.com/share" class="jiathis jiathis_txt jtico jtico_jiathis" target="_blank"></a>
     </div>
-    <!-- JiaThis Button END -->
   </section>
 </template>
 <script>
@@ -19,7 +16,7 @@
     mounted () {}
   }
 </script>
-<style scoped lang="less">
+<style lang="less">
   @import "../assets/var.less";
 
   .share-btn {
@@ -27,9 +24,25 @@
       display: inline-block;
       vertical-align: middle;
     }
-    .jiathis_style_32x32{
+    .jiathis_style_32x32 {
+      background: none;
       display: inline-block;
       vertical-align: middle;
+      .jiathis_txt {
+        background: none;
+      }
+      .jtico_tsina {
+        background: url("../static/weibo.png") no-repeat center;
+      }
+      .jtico_douban {
+        background: url("../static/douban.png") no-repeat center;
+      }
+      .jtico_huaban {
+        background: url("../static/huaban.png") no-repeat center;
+      }
+      .jtico_cqq {
+        background: url("../static/qq.png") no-repeat center;
+      }
     }
   }
 </style>
